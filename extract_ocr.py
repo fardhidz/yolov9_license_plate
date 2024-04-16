4bd02ed1917029819febb8725aa442c72da0df19
FARDHI DZAKWAN FAUZAN <222112042@stis.ac.id>
2024-04-16 13:39:33 +0800

    ubah extract ocr 2

--- extract_ocr.py
+++ extract_ocr.py
@@ -212,7 +212,7 @@ def run(
 
 def parse_opt():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--weights', nargs='+', type=str, default=ROOT / 'content/yolov9/runs/train/yolov9-c/weights/best.pt', help='model path or triton URL')
+    parser.add_argument('--weights', nargs='+', type=str, default=ROOT / 'yolov9-c/weights/best.pt', help='model path or triton URL')
     parser.add_argument('--source', type=str, default=ROOT / 'Data Test', help='file/dir/URL/glob/screen/0(webcam)')
     parser.add_argument('--data', type=str, default=ROOT / 'yolov9_license_plate/data.yaml', help='(optional) dataset.yaml path')
     parser.add_argument('--imgsz', '--img', '--img-size', nargs='+', type=int, default=[640], help='inference size h,w')
